c2b03588aa29d689798f989f6eb9cba8c34788fc
Aaron Turner <aaron@aaronthedev.com>
2019-11-21 00:16:36 -0800

    Did some better styling, and other improvments

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -4,6 +4,35 @@ import { Component } from "preact";
 
 export default class About extends Component {
   render() {
-    return <div class="about">About</div>;
+    return (
+      <div class="about">
+        About
+        <div>
+          Made by{" "}
+          <a href="https://aaronthedev.com/">Aaron Turner (torch2424)</a> with
+          help from <a href="https://www.loxodrome.io/">James Milner</a> and
+          some{" "}
+          <a href="https://github.com/torch2424/made-with-webassembly/graphs/contributors">
+            Amazing Contributors
+          </a>
+          .
+        </div>
+        <div class="footer__additional-links">
+          <div>
+            <a href="/all-projects">All Projects</a>
+          </div>
+          <div>
+            <a href="https://github.com/torch2424/made-with-webassembly">
+              Source Code (Github)
+            </a>
+          </div>
+          <div>
+            <a href="https://oss.ninja/mit?organization=Aaron%20Turner">
+              License
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
