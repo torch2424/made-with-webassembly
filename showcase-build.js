b8c371f4eb532fc7437c71c4d06cdb39fbb58c4a
Aaron Turner <aaron@aaronthedev.com>
2020-01-26 23:17:40 -0800

    Got a createdDate key for all projects in the showcaseJson

--- showcase-build.js
+++ showcase-build.js
@@ -4,6 +4,11 @@ const path = require("path");
 const mkdirp = require("mkdirp");
 const recursiveReadDir = require("recursive-readdir");
 const recursiveCopy = require("recursive-copy");
+
+// Require our command execution for git
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
 // Require our Markdown parsing
 const showdown = require("showdown");
 const showdownConverter = new showdown.Converter({ metadata: true });
@@ -37,9 +42,23 @@ const buildTask = async () => {
     }
   ];
 
-  showcaseFiles.forEach(file => {
+  for (let i = 0; i < showcaseFiles.length; i++) {
+    const file = showcaseFiles[i];
+
     // Get the contents of the file
     const text = fs.readFileSync(file, "utf8");
+
+    // Get the date when the file was added to the repo, and format it
+    // to only display the date
+    // https://stackoverflow.com/questions/11533199/git-find-commit-where-file-was-added
+    // https://stackoverflow.com/questions/22497597/get-the-last-modification-data-of-a-file-in-git-repo
+    let gitAddedToRepoDate;
+    const { stdout } = await exec(
+      `git log --diff-filter=A --pretty="format:%ci" -- ${file}`
+    );
+    if (stdout) {
+      gitAddedToRepoDate = new Date(stdout);
+    }
 
     // Get the name, html, and metadata of the file
     const name = path.basename(file, ".md");
@@ -48,6 +67,11 @@ const buildTask = async () => {
 
     // Add the metadata
     metadata["key"] = name;
+    if (gitAddedToRepoDate) {
+      metadata["createdDate"] = gitAddedToRepoDate.valueOf();
+    }
+
+    // Add the metadata to our metadata JSON we will be saving to our showcase.json
     metadataJson[name] = metadata;
 
     // Add the Prerender Data
@@ -59,7 +83,7 @@ const buildTask = async () => {
 
     // Create the HTML output
     fs.writeFileSync(`./src/assets/showcase/${name}.html`, html);
-  });
+  }
 
   // Create our showcase json (minified)
   fs.writeFileSync(`./src/assets/showcase.json`, JSON.stringify(metadataJson));
