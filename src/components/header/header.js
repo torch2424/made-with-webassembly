b06015fb699cc799d81ce946a176ea226113bc1e
Aaron Turner <aaron@aaronthedev.com>
2022-06-01 19:35:11 -0700

    Added Announcements & Removed Pajama Sam 3

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,9 +3,47 @@ import { Component } from "preact";
 // Header nav bar for the app
 
 export default class Header extends Component {
+  constructor() {
+    super();
+
+    this.announcementText = (
+      <p>
+        The maintainer of this website has a{" "}
+        <a
+          href="https://open.spotify.com/playlist/2L8Gm7T2aJvRj1ih03UeKv"
+          target="_blank"
+        >
+          Spotify Coding Playlist of their Lo-fi Hip Hop beats
+        </a>
+        !
+      </p>
+    );
+
+    const cascadiaDate = new Date(2022, 8, 30);
+    if (new Date().getTime() < cascadiaDate.getTime()) {
+      this.announcementText = (
+        <p>
+          The maintainer of this website is{" "}
+          <a href="https://2022.cascadiajs.com/tickets/79" target="_blank">
+            attending and emcee-ing CascadiaJS
+          </a>
+          , and has a{" "}
+          <a
+            href="https://open.spotify.com/playlist/2L8Gm7T2aJvRj1ih03UeKv"
+            target="_blank"
+          >
+            Spotify Coding Playlist of their Lo-fi Hip Hop beats
+          </a>
+          !
+        </p>
+      );
+    }
+  }
+
   render() {
     return (
       <div class="header">
+        <div class="announcements">{this.announcementText}</div>
         <nav class="nav">
           <div class="nav__left">
             <a class="nav__title" href="/">
